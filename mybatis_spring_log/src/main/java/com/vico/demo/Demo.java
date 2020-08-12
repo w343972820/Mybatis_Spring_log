0ab779f20ec4e86e24557a0ff8a71e61b06f2189
w343972820 <343972820@qq.com>
2020-08-12 18:29:35 +0800

    提交日志输出

---
+++ mybatis_spring_log/src/main/java/com/vico/demo/Demo.java
@@ -0,0 +1,62 @@
+package com.vico.demo;
+
+
+import com.vico.model.UserSms;
+import com.vico.utils.DatabaseUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+
+
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+
+
+@RestController
+@RequestMapping("v1")
+@Api(value = "v1",description = "这是我的第一个版本的demo")
+public class Demo {
+    private SqlSession sqls;
+    private Logger logger= LoggerFactory.getLogger(Demo.class);
+    public Demo() throws IOException {
+        this.sqls = DatabaseUtil.getSqlSession();
+    }
+    //数据库查询
+    @RequestMapping(value = "/getUserPhone",method = RequestMethod.GET)
+    @ApiOperation(value = "获取用户电话号码",httpMethod = "GET")
+    public String getUserPhone() throws IOException {
+        String phone = sqls.selectOne("vicos1.getUserCount", 70);
+        return phone+" ..正常....";
+    }
+    //数据库新增
+    @RequestMapping(value="/addSms",method = RequestMethod.POST)
+    public int addSms(@RequestBody UserSms sms){
+        int result=sqls.insert("vicos1.addSms",sms);
+        sqls.commit();
+        return result;
+    }
+    //数据库修改
+    @RequestMapping(value="/updateSms",method = RequestMethod.POST)
+    public int updateSms(@RequestBody UserSms sms){
+        int result=sqls.insert("vicos1.updateSam",sms);
+        sqls.commit();
+        return result;
+    }
+    //数据库删除
+    @RequestMapping(value="/deleteSms",method = RequestMethod.POST)
+    public int deleteSms(@RequestParam int id){
+        int result=sqls.insert("vicos1.deleteSam",id);
+        sqls.commit();
+        return result;
+    }
+    @RequestMapping(value = "lookeLog",method = RequestMethod.GET)
+    public void lookLog(){
+        logger.info("==================");
+    }
+}
